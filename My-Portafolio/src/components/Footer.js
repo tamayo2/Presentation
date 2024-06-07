d750aaa55339a23fcca2d52fd5e3c788a9064e2d
tamayo2 <juan.tamayo3@utp.edu.co>
2024-06-07 14:04:25 -0500

    V1

--- My-Portafolio/src/components/Footer.js
+++ My-Portafolio/src/components/Footer.js
@@ -17,10 +17,10 @@ export const Footer = () => {
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.facebook.com/profile.php?id=100074108005824&mibextid=LQQJ4d"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/juan_tamayo21?igsh=MXAxMXBqdWU2bDlkYg=="><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2024. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
